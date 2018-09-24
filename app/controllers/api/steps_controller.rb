c6833806fc3df9cf9402388276af78004190b5fb
lucas <lucasprograms@gmail.com>
2018-09-23 22:41:01 -0400

    remove debugger :|

--- app/controllers/api/steps_controller.rb
+++ app/controllers/api/steps_controller.rb
@@ -6,7 +6,6 @@ class Api::StepsController < ApplicationController
   def create
     @step = Step.new(step_params)
     @step.todo_id = params[:todo_id]
-    debugger
 
     if @step.save
       render json: @step
